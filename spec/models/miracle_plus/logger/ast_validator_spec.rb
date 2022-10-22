e3228716614401fdea53d273d8690769ef2cd0bc
Hunter <ttitoo@gmail.com>
2022-10-22 09:25:02 +0000

    test:

---
+++ spec/models/miracle_plus/logger/ast_validator_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+RSpec.describe MiraclePlus::Logger::AstValidator, type: :service do
+  describe '#initialize' do
+    context 'when code is nil' do
+      it 'raise error' do
+        expect { described_class.new(nil) } .to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when code is empty' do
+      it 'raise error' do
+        expect { described_class.new('') }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when code is present' do
+      it 'create the instance' do
+        expect { described_class.new('a = 1') }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#illegal_operation?' do
+    context 'when code contains shell command' do
+      it 'raise InvalidStatementError' do
+        expect { described_class.new('`ls /`').perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+
+      it 'raise InvalidStatementError' do
+        expect { described_class.new('%x|ls /|').perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+
+      it 'raise InvalidStatementError' do
+        expect { described_class.new("system('ls /')").perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+    end
+
+    context 'when code contains kernel method' do
+      it 'riase InvalidStatementError' do
+        expect { described_class.new('fork { puts 1 }').perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+
+      it 'riase InvalidStatementError' do
+        expect { described_class.new('raise 123').perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+    end
+
+    context 'when code contains forbidden classes' do
+      it 'riase InvalidStatementError' do
+        expect { described_class.new("File.open('./log/test.log')").perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+
+      it 'riase InvalidStatementError' do
+        expect { described_class.new("Kernel.raise('test')").perform }.to raise_error(MiraclePlus::Logger::InvalidStatementError)
+      end
+    end
+  end
+end
--- spec/models/miracle_plus/logger/logging_spec.rb
+++ spec/models/miracle_plus/logger/logging_spec.rb
@@ -2,7 +2,15 @@
 
 require 'rails_helper'
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 RSpec.describe MiraclePlus::Logger::Concerns::Logging, type: :model do
+  let(:io) do
+    {
+      args: [1, 2, 3],
+      result: 10
+    }
+  end
   describe '#initialize' do
     context 'when has no parameters' do
       subject do
@@ -49,6 +57,48 @@ RSpec.describe MiraclePlus::Logger::Concerns::Logging, type: :model do
 
       it 'create the instance' do
         expect { subject.new([1, 2, 3]) }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#exec_in_transaction' do
+    let(:instance) do
+      Class.new do
+        include MiraclePlus::Logger::Concerns::Logging
+      end.new
+    end
+    subject do
+      instance.send(:exec_in_transaction, statements, io)
+    end
+
+              # File.delete("#{Rails.root}/tmp/" + '1.test')
+              # Kernel.raise('')
+              # Dir.empty?("#{Rails.root}/tmp")
+              # proc { `ls #{Rails.root}/tmp` }.call
+              # ap `ls #{Rails.root}/tmp` && a = args.first
+              # ap `ls ./`; b = args.last
+
+    context 'when contains rm command' do
+      let(:file) { "#{Rails.root}/tmp/1.test" }
+      let(:statements) do
+        [
+          {
+            variable: 'name',
+            code: <<-RUBY
+              %x|ls|
+              a = 100
+              result * a
+            RUBY
+          }.with_indifferent_access
+        ]
+      end
+
+      it 'will not delete the file' do
+        FileUtils.touch(file)
+        ap File.file?(file)
+        ap subject
+        ap File.file?(file)
+        expect(File.file?(file)).to eq(true)
       end
     end
   end
