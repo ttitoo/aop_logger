1316906e1f6dcd6e93764d99d8be6b73d1da8498
Hunter <ttitoo@gmail.com>
2022-11-10 02:23:28 +0000

    feat: get ip from header

--- lib/miracle_plus/logger/context_middleware.rb
+++ lib/miracle_plus/logger/context_middleware.rb
@@ -11,10 +11,11 @@ module MiraclePlus
 
       def call(env)
         user_id = current_user_id(env)
-        entries = Entry.list(ip: env['REMOTE_ADDR'])
+        ip = request_ip(env)
+        entries = Entry.list(ip: ip)
         ids = uniq_ids.prepend(user_id || 0)
         RequestStore.store = {
-          request_ip: env['REMOTE_ADDR'],
+          request_ip: ip,
           need_tracking: entries.present?,
           entries: entries,
           logging: MiraclePlus::Logger::Instance.pop(track_id: hashids.encode(*ids), current_user_id: user_id),
@@ -49,6 +50,10 @@ module MiraclePlus
                 .split('.')
                 .prepend(Random.new.rand(1_000_000))
                 .map(&:to_i)
+      end
+
+      def request_ip(env)
+        @request_ip ||= env['HTTP_X_FORWARDED_FOR']
       end
     end
   end
